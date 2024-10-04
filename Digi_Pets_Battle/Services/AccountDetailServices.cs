2a23017b947a96a22377ace602c35068ffe6cfd0
Martina Kay Basquez <martinabasquez@gmail.com>
2024-10-03 20:30:04 -0500

    attempt at the services -- stay tuned

--- Digi_Pets_Battle/Services/AccountDetailServices.cs
+++ Digi_Pets_Battle/Services/AccountDetailServices.cs
@@ -12,10 +12,22 @@ public class AccountDetailServices
         _httpClient.BaseAddress = new Uri("http://localhost:8080/accounts");
     }
 
-    public async Task<AccountDetails> GetAll()
+    public async Task<AccountDetails> GetAccountDetailsByApiKey(string apiKey)
     {
-        AccountDetails result = await _httpClient.GetFromJsonAsync<AccountDetails>("api/Pet");
+        AccountDetails result = await _httpClient.GetFromJsonAsync<AccountDetails>($"/by-key/{apiKey}");
         return (result);
+    }
+
+    public async Task<AccountDetails> GetAccountDetails(long id, string apiKey)
+    {
+        AccountDetails result = await _httpClient.GetFromJsonAsync<AccountDetails>($"?username={id}n&apiKey={apiKey}");
+        return result;
+    }
+
+    public async Task<AccountDetails> GettingAction(string action, string apiKey)
+    {
+        AccountDetails result = await _httpClient.GetFromJsonAsync<AccountDetails>($"/by-key/{apiKey}/action?action={action}");
+        return result;
     }
 }
     
